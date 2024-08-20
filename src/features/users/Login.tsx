a6888eff87a26d38175b686c379a3d42497bdf6c
azatbek <sadykowazatbek@gmail.com>
2024-08-20 13:45:32 +0600

    feat: added the ability to register and login

---
+++ src/features/users/Login.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Alert,
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { LoadingButton } from '@mui/lab';
+import { appRoutes, regEx } from '../../utils/constants';
+
+import { login } from './usersThunks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  selectLoginError,
+  selectLoginLoading,
+  setLoginError,
+} from './usersSlice';
+import { LoginMutation } from '../../types/types.User';
+import InputAdornment from '@mui/material/InputAdornment';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Visibility from '@mui/icons-material/Visibility';
+
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const error = useAppSelector(selectLoginError);
+  const loading = useAppSelector(selectLoginLoading);
+
+  const [state, setState] = useState<LoginMutation>({
+    email: '',
+    password: '',
+  });
+
+  const [showPass, setShowPass] = useState(false);
+  const [emailIsValid, setEmailIsValid] = useState<boolean>(true);
+  const [emailLabel, setEmailLabel] = useState<string>('');
+
+  const handleClickShowPassword = () => setShowPass((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setState((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const submitFormHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      if (regEx.test(state.email)) {
+        setEmailIsValid(true);
+      } else if (!regEx.test(state.email) && state.email !== '') {
+        setEmailLabel('Неверный формат электронной почты!');
+        setEmailIsValid(false);
+        return;
+      }
+
+      await dispatch(login(state)).unwrap();
+      navigate(appRoutes.home);
+    } catch (e) {
+      setEmailIsValid(false);
+      setEmailLabel('Неверный e-mail или пароль, пожалуйста повторите!');
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(setLoginError(null));
+  }, [dispatch]);
+
+  return (
+    <Container component="main" maxWidth="lg">
+      <Grid container alignItems="center">
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Войти в личный кабинет
+            </Typography>
+
+            {error && (
+              <Grid item xs={12}>
+                <Alert severity="error" sx={{ mt: 3, width: '100%' }}>
+                  {error.message}
+                </Alert>
+              </Grid>
+            )}
+
+            <Box component="form" onSubmit={submitFormHandler} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    required
+                    label="Email"
+                    name="email"
+                    type="email"
+                    value={state.email}
+                    placeholder="email@email.com"
+                    autoComplete="current-email"
+                    onChange={inputChangeHandler}
+                    error={!emailIsValid}
+                    helperText={emailLabel}
+                    sx={{
+                      '.MuiFormHelperText-root': {
+                        color: emailIsValid ? 'inherit' : '#d32f2f',
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="password"
+                    label="Пароль"
+                    type={showPass ? 'text' : 'password'}
+                    fullWidth
+                    autoComplete="current-password"
+                    value={state.password}
+                    onChange={inputChangeHandler}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPass ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid item xs={12} textAlign="center">
+                <LoadingButton
+                  type="submit"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, py: 1 }}
+                  disableElevation
+                  disabled={loading}
+                  loading={loading}
+                >
+                  Войти
+                </LoadingButton>
+              </Grid>
+              <Grid item xs={12} textAlign="center">
+                <Link
+                  component={RouterLink}
+                  to={appRoutes.register}
+                  variant="body2"
+                >
+                  Еще нет аккаунта? Регистрация
+                </Link>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/features/users/Register.tsx
@@ -0,0 +1,241 @@
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {
+  selectRegisterError,
+  selectRegisterLoading,
+  setRegisterError,
+} from './usersSlice';
+import { register } from './usersThunks';
+import { appRoutes, regEx } from '../../utils/constants';
+import InputAdornment from '@mui/material/InputAdornment';
+import { RegisterMutation } from '../../types/types.User';
+
+const initialState: RegisterMutation = {
+  email: '',
+  password: '',
+  displayName: '',
+};
+
+const Register: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const error = useAppSelector(selectRegisterError);
+  const loading = useAppSelector(selectRegisterLoading);
+
+  const [state, setState] = useState<RegisterMutation>(initialState);
+  const [showPass, setShowPass] = useState(false);
+  const [passLabel, setPassLabel] = useState<string>(
+    'Длина пароля должна быть не менее 8 символов',
+  );
+  const [passIsValid, setPassIsValid] = useState<boolean | undefined>(
+    undefined,
+  );
+  const [emailIsValid, setEmailIsValid] = useState<boolean>(true);
+  const [emailLabel, setEmailLabel] = useState<string>('');
+
+  const getFieldError = (fieldName: string) => {
+    try {
+      return error?.errors[fieldName].message;
+    } catch {
+      return undefined;
+    }
+  };
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setState((prevState) => {
+      if (name === 'password' && value.length >= 8) {
+        setPassIsValid(true);
+        setPassLabel('Подходящий пароль');
+      }
+
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const isFormValid = () => {
+    return (
+      state.email && state.password
+    );
+  };
+
+  const handleClickShowPassword = () => setShowPass((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    dispatch(setRegisterError(null));
+  }, [dispatch]);
+
+  const submitFormHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      if (state.password.length >= 1 && state.password.length < 8) {
+        setPassLabel('Пароль слишком короткий!');
+        setPassIsValid(false);
+        return;
+      }
+
+      if (regEx.test(state.email)) {
+        setEmailIsValid(true);
+      } else if (!regEx.test(state.email) && state.email !== '') {
+        setEmailLabel('Неверный формат электронной почты!');
+        setEmailIsValid(false);
+        return;
+      }
+
+      await dispatch(register(state)).unwrap();
+      navigate(appRoutes.home);
+      setState(initialState);
+    } catch (e) {
+      setEmailIsValid(false);
+      setEmailLabel('Такой пользователь уже существует!');
+      console.error(e);
+    }
+  };
+
+  return (
+    <Container component="main">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Регистрация
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={submitFormHandler}
+          sx={{ mt: 3, width: '100%' }}
+        >
+          <Grid container spacing={2} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                required
+                label="Email"
+                name="email"
+                type="email"
+                value={state.email}
+                placeholder="email@email.com"
+                autoComplete="new-email"
+                onChange={inputChangeHandler}
+                error={Boolean(getFieldError('email') || !emailIsValid)}
+                helperText={
+                  getFieldError('email') ? getFieldError('email') : emailLabel
+                }
+                sx={{
+                  '.MuiFormHelperText-root': {
+                    color: emailIsValid ? 'inherit' : '#d32f2f',
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                required
+                name="displayName"
+                label="Отображаемое имя"
+                type="text"
+                value={state.displayName}
+                onChange={inputChangeHandler}
+              />
+
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                required
+                name="password"
+                label="Пароль"
+                type={showPass ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPass ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                value={state.password}
+                autoComplete="new-password"
+                onChange={inputChangeHandler}
+                error={Boolean(
+                  getFieldError('password') || passIsValid === false,
+                )}
+                helperText={
+                  getFieldError('password')
+                    ? getFieldError('password')
+                    : passLabel
+                }
+                sx={{
+                  '.MuiFormHelperText-root': {
+                    color: passIsValid ? 'primary.main' : 'inherit',
+                  },
+                }}
+              />
+            </Grid>
+            <Grid
+              container
+              item
+              direction="column"
+              justifyContent="center"
+              lg={12}
+            >
+              <Grid item>
+              </Grid>
+              <Grid item textAlign="center">
+                <LoadingButton
+                  type="submit"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, py: 1 }}
+                  disableElevation
+                  disabled={!isFormValid() || loading}
+                  loading={loading}
+                >
+                  Зарегистрироваться
+                </LoadingButton>
+              </Grid>
+              <Grid item textAlign="center">
+                <Link
+                  component={RouterLink}
+                  to={appRoutes.login}
+                  variant="body2"
+                >
+                  Уже зарегистрированы? Войти
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+export default Register;
